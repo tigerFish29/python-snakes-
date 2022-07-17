a5efcb47a4f2795c481edc82aefdae30ca0f8fc6
tigerFish29 <paulwchabz28@gmail.com>
2022-07-17 22:14:20 +0100

    made changes to main.py

--- main.py
+++ main.py
@@ -39,8 +39,24 @@ class Food:
                            y_coordinate + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
 
 
-def next_turn():
-    pass
+def next_turn(snake, turn):
+    x, y = snake.coordinates[0]
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+    # reformat the coordinates
+    snake.coordinates.insert(0, (x, y))
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+    snake.squares.insert(0, square)
+    del snake.coordinates[-1]
+    canvas.delete(snake.squares[-1])
+    del snake.squares[-1]
+    window.after(SPEED, next_turn, snake, food)
 
 
 def change_direction(new_direction):
@@ -83,7 +99,14 @@ y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('Up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
 snake = Snake()
 food = Food()
+
+next_turn(snake, food)
 
 window.mainloop()
