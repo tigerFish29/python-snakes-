f187c1d95e64c0b36725ee082b85ffcbb95a083b
tigerFish29 <paulwchabz28@gmail.com>
2022-07-17 21:18:43 +0100

    made changes to food class

--- main.py
+++ main.py
@@ -12,25 +12,35 @@ FOOD_COLOR = "yellow"
 BACKGROUND_COLOR = "#000000"
 
 
-
-
 class Snake:
     pass
 
+
 class Food:
-    pass
+    def __init__(self):
+        x_coordinate = random.randint(0, 700)
+        y_coordinate = random.randint(0, 700)
+
+        self.coordinates = [x_coordinate, y_coordinate]
+        # draw food canvas
+        canvas.create_oval(x_coordinate, y_coordinate, x_coordinate + SPACE_SIZE,
+                           y_coordinate + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
+
 
 def next_turn():
-  pass
+    pass
+
 
 def change_direction(new_direction):
-  pass
+    pass
+
 
 def check_collisions():
-  pass
+    pass
+
 
 def game_over():
-  pass
+    pass
 
 
 window = Tk()
@@ -47,5 +57,21 @@ label.pack()
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
-window.mainloop()
+window.update()
 
+window_height = window.winfo_height()
+window_width = window.winfo_width()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# adjust the position of window
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+# set the geometry of the window
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+window.mainloop()
