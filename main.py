4288f153d5582bbdade55d8f44d8db50afbda5c2
tigerFish29 <paulwchabz28@gmail.com>
2022-07-17 22:33:41 +0100

    made changes to app

--- main.py
+++ main.py
@@ -60,7 +60,23 @@ def next_turn(snake, turn):
 
 
 def change_direction(new_direction):
-    pass
+    global direction
+
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
 
 
 def check_collisions():
